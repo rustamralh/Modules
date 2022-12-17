0ac68803d12da85204bd3f5f8511a6dce780abd7
Rustam <rustam@xyrintech.com>
2022-12-17 13:59:46 +0530

    initial commit

---
+++ Modules/Spotify/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('spotify::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('spotify.name') !!}
+    </p>
+@endsection
